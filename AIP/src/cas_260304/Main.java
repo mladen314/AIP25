c20fae6912ae768c78a186f1b293ad433b916e7b
mladen314 <144899141+mladen314@users.noreply.github.com>
2026-03-04 10:15:12 +0100

    Create Main.java

---
+++ AIP/src/cas_260304/Main.java
@@ -0,0 +1,58 @@
+package cas_260304;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList <String> osobe = new ArrayList<>();
+		
+		osobe.add("Ivan");
+		osobe.add("Tijana");
+		osobe.add(1, "Matija");
+		osobe.add("Cadjo");
+		osobe.add("Petar");
+		
+		System.out.println(osobe.size());
+		System.out.println(osobe);
+		
+		System.out.println(osobe.get(0).toUpperCase());
+		osobe.remove(0);
+		System.out.println(osobe);
+		System.out.println(osobe.get(0));
+		osobe.remove("Cadjo");
+		System.out.println(osobe);
+		System.out.println(osobe.contains("Petar"));
+		
+		for (String osoba : osobe) {
+			System.out.println(osoba);
+		}
+		
+		for (int i = 0; i < osobe.size(); i++) {
+			System.out.println(osobe.get(i));
+		}
+		
+		osobe.set(1, "Biga");
+		System.out.println(osobe);
+
+	}
+	
+	/*
+	 * Napisati program koji unosi imena dko se ne unese KRAJ
+	 * Nakon zavrsetka unosa ispisati listu
+	 */
+	
+	public static void popuni_listu(ArrayList <String> osobe) {
+		Scanner input = new Scanner(System.in);
+		String ime;
+		do {
+			
+		}while(!ime.equalsIgnoreCase("kraj"));
+	}
+
+}
