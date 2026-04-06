8d12082c019ddf53fa8e92bb677c4784c382d2e3
mladen314 <144899141+mladen314@users.noreply.github.com>
2026-04-06 17:30:12 +0200

    260406

---
+++ AIP/src/cas_260406_OOP/Main.java
@@ -0,0 +1,15 @@
+package cas_260406_OOP;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Osobe a = new Osobe(123456789, "Marko", "Markovic", 2000);
+		Osobe b = new Osobe(987654321, "Zarko", "Zarkovic", 2005);
+		
+		System.out.println(a.ime + " " + a.prezime);
+
+	}
+
+}
---
+++ AIP/src/cas_260406_OOP/Osobe.java
@@ -0,0 +1,19 @@
+package cas_260406_OOP;
+
+public class Osobe {
+	long jmbg;
+	String ime;
+	String prezime;
+	int godine_rodjenja;
+	
+	public Osobe(long jmbg, String ime, String prezime, int godine_rodjenja) {
+		this.jmbg = jmbg;
+		this.ime = ime;
+		this.prezime = prezime;
+		this.godine_rodjenja = godine_rodjenja;
+	}
+	
+	
+	
+
+}
